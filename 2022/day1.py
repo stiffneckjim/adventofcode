c9d98fda13707954de8328e150f6a642942d38b7
Stiff Neck Jim <22213990+stiffneckjim@users.noreply.github.com>
2022-12-06 20:26:18 +0000

    Correct answer for first puzzle.

---
+++ 2022/day1.py
@@ -0,0 +1,20 @@
+with open("input.txt", "r") as f:
+    lines = f.readlines()
+
+high_score = 0
+total = 0
+
+for line in lines:
+    line = line.replace("\n", "")
+    # print(line)
+    if line == "":
+        if total > high_score:
+            high_score = total
+
+        total = 0
+    else:
+
+        total += int(line)
+        # print(f"Total: {total}")
+
+print(f"High score: {high_score}")
