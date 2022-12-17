03e0b474963c29e32c0f8f3c1be82e58e6e4c5fb
Stiff Neck Jim <22213990+stiffneckjim@users.noreply.github.com>
2022-12-17 15:01:56 +0000

    Solved day 1 part 2

--- 2022/day 1 part 2.py
+++ 2022/day 1 part 2.py
@@ -1,15 +1,17 @@
 with open("input.txt", "r") as f:
     lines = f.readlines()
 
-high_score = 0
-totals = [0, 0, 0]
+total = 0
+high_scores = [0, 0, 0]
 
 for line in lines:
     line = line.replace("\n", "")
-    # print(line)
+    # This only works because there's an empty line at the end of the file
     if line == "":
-        if total > high_score:
-            high_score = total
+        if total > high_scores[0]:
+            high_scores[0] = total
+            high_scores = sorted(high_scores)
+            print(f"High scores: {high_scores[0]}, {high_scores[1]}, {high_scores[2]}")
 
         total = 0
     else:
@@ -17,5 +19,4 @@ for line in lines:
         total += int(line)
         # print(f"Total: {total}")
 
-print(f"High score total: {high_score}")
-
+print(f"High scores total: {sum(high_scores)}")
