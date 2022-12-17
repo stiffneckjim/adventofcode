633127a646acab5a25a64fa409620fa4b01b08e7
Stiff Neck Jim <22213990+stiffneckjim@users.noreply.github.com>
2022-12-17 21:21:11 +0000

    Working on test data.

---
+++ 2022/day 3 part 1.py
@@ -0,0 +1,38 @@
+def lookup_priority(char):
+    return "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".index(char) + 1
+
+
+# Take a string and split it into two lists
+def split_line(line):
+    length = len(line) // 2
+    print(f"Length: {length}")
+    return list(line)[:length], list(line)[length:]
+
+
+def find_duplicate(left, right):
+    for i in range(len(left)):
+        for j in range(len(right)):
+            if left[i] == right[j]:
+                return left[i]
+
+
+# with open("day 3 input.txt", "r") as f:
+with open("test.txt", "r") as f:
+    lines = f.readlines()
+
+
+total = 0
+
+for line in lines:
+    line = line.replace("\n", "")
+    print(f"Input: {line}")
+    left, right = split_line(line)
+    print(f"Left: {left}, Right: {right}")
+
+    char = find_duplicate(left, right)
+    print(f"Duplicate: {char}")
+    score = lookup_priority(char)
+    total += score
+    print(f"Score: {score}, Total: {total}")
+
+print(f"Total: {total}")
