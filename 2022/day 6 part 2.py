cd950323243d7e149f07ebf8de9f7e183f3e89c1
Stiff Neck Jim <22213990+stiffneckjim@users.noreply.github.com>
2022-12-20 19:49:52 +0000

    Solved day 6 part 2. One character change from part 1.

---
+++ 2022/day 6 part 2.py
@@ -0,0 +1,26 @@
+def find_dupes(str):
+
+    if len(str) == 1:
+        print(f"Not found")
+        return False
+    print(f"Search {str[1:]} for {str[0]}")
+
+    if str.find(str[0], 1) >= 0:
+        return True
+    else:
+        return find_dupes(str[1:])
+
+
+# with open("test.txt", "r") as f:
+with open("day 6 input.txt", "r") as f:
+    lines = f.readlines()
+
+for line in lines:
+
+    line = line.replace("\n", "")
+    for i in range(len(line) - 3):
+        if find_dupes(line[i : i + 14]) == False:
+            print(f"Result: {i + 14}")
+            exit()
+
+print("No marker found!")
